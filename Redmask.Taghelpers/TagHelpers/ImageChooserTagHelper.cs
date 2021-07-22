12cd3595b4e35452ea851e39230ffbacc2833a48
Vahid Aria <Vahid@ARIA-LAPTOP>
2021-07-22 11:11:14 +0430

    a

---
+++ Redmask.Taghelpers/TagHelpers/ImageChooserTagHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Redmask.Taghelpers.TagHelpers
+{
+    [HtmlTargetElement("imageChooser", Attributes = "asp-for", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class ImageChooserTagHelper : TagHelper
+    {
+        [HtmlAttributeName("asp-for")]
+        public ModelExpression Model { get; set; }
+
+        public string FolderPath { get; set; }
+        public int MaxAllowedKb{ get; set; } = 80;
+        public double MinRatioHeightToWidth { get; set; } = .001;
+        public double MaxRatioHeightToWidth { get; set; } = 100;
+        public string DefaultAvatar { get; set; } = "/noIimage268.png";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var src = Model.Model != null ? FolderPath + Model.Model : DefaultAvatar;
+            var imgId =Model.Name+ Guid.NewGuid().ToString().Split('-')[0];
+            var fileId = Model.Name + "file";
+            var maxAllowedKillobyte = (MaxAllowedKb+6) * 1000;
+
+            output.Content.SetHtmlContent(
+                 "<img src='" + src + "' id='" + imgId + "' style='height:100%; cursor:pointer;' />" +
+                 "<input type='file' name='"+ fileId+ "' id='" + fileId + "' style='display:none' accept='image/*' />" +
+                 "<script>" +
+                 "            $('#" + imgId + "').click(function () { $('#"+ fileId+"').trigger('click'); });" +
+                 "            $('#" + fileId + "').on('change', function (evt) {                                                       " +
+                 "                var tgt = evt.target || window.event.srcElement,                                           " +
+                 "                    files = tgt.files;                                                                     " +
+                 "                if (FileReader && files && files.length) {                                                 " +
+                 "                                                                                                           " +
+                 "                    if (files[0].size > "+ maxAllowedKillobyte + ") {                                                           " +
+                 "                        alert('حجم عکس باید کمتر از  " + MaxAllowedKb + " کیلوبایت باشد');                                " +
+                 "                        $('#" + fileId + "').val('');                                                           " +
+                 "                        return;                                                                            " +
+                 "                    };                                                                                     " +
+                 "                    var fr = new FileReader();                                                             " +
+                 "                    fr.onload = function () {                                                              " +
+                 "                                                                                                           " +
+                 "                        var image = new Image();                                                           " +
+                 "                        image.src = fr.result;                                                             " +
+                 "                        image.onload = function () {                                                       " +
+                 "                            var height = this.height;                                                      " +
+                 "                            var width = this.width;                                                        " +
+                 "                            if (!(height >= "+ MinRatioHeightToWidth+ " * width && height <= " + MaxRatioHeightToWidth + " * width)) {                         " +
+                 "                                alert('ارتفاع تصویر باید بین "+ MinRatioHeightToWidth+" تا "+ MaxRatioHeightToWidth+" برابر عرض تصویر باشد');           " +
+                 "                                $('#"+ fileId+"').val('');                                                   " +
+                 "                                $('#s-btn').css('display', 'none');                                        " +
+                 "                                return false;                                                              " +
+                 "                            }                                                                              " +
+                 "                                                                                                           " +
+                 "                            document.getElementById('"+ imgId + "').src = fr.result;                      " +
+                 "                            $('#s-btn').css('display', '');                                                " +
+                 "                            return true;                                                                   " +
+                 "                        };                                                                                 " +
+                 "                    };                                                                                     " +
+                 "                    fr.readAsDataURL(files[0]);                                                            " +
+                 "                }                                                                                          " +
+                                  // Not supported
+                 "                else {                                                                                     " +
+                 //                      fallback -- perhaps submit the input to an iframe and temporarily store
+                 //                      them on the server until the user's session ends.
+                 "                };                                                                                         " +
+                 "            });                                                                                            " +
+                 "</script>"
+                );
+
+
+
+        }
+    }
+
+}
---
+++ Redmask.Taghelpers/TagHelpers/PersianDatePickerTagHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Redmask.Taghelpers.TagHelpers
+{
+    [HtmlTargetElement("persianDatePicker", Attributes = DescriptionAttributeName, TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class PersianDatePickerTagHelper : TagHelper
+    {
+        private const string DescriptionAttributeName = "asp-for";
+
+        [HtmlAttributeName(DescriptionAttributeName)]
+        public ModelExpression Model { get; set; }
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var dt = Model.Model != null ? (DateTime)Model.Model : DateTime.Now;
+            dt = dt == DateTime.MinValue ? DateTime.Now : dt;
+            var enabletimepicker = "true";
+            output.Content.SetHtmlContent(
+                 "<div class='input-group'" +
+                 ">" +
+                 "   <div class='input-group-addon'                           " +
+                 "     data-mddatetimepicker='true'                           " +
+                 "     data-targetselector='#" + Model.Name + "'              " +
+                 "     data-trigger='click'                                   " +
+                 "     data-enabletimepicker='"+enabletimepicker+"'           " +
+                 "     data-isgregorian='false'                               " +
+                 "     data-placement='bottom'                                " +
+                 "     style='padding: 2px 12px;'>                            " +
+                 "       <span class='la la-calendar la-2x'></span>           " +
+                 "   </div>                                                   " +
+                 "   <input type = 'text' class='form-control' id='prefix" + Model.Name + "' value='" + ToPersian(dt) + "'" +
+                 "        data-mddatetimepicker='true'                        " +
+                 "        data-targetselector='#" + Model.Name + "'           " +
+                 "        data-trigger='click'                                " +
+                 "        data-enabletimepicker='" + enabletimepicker + "'    " +
+                 "        data-isgregorian='false'                            " +
+                 "        data-placement='bottom'                             " +
+                 "        data-englishnumber='false'                          " +
+                 "        readonly           />" +
+                 "   <input type='hidden' id='" + Model.Name + "' name='" + Model.Name + "' value='" + dt + "' />" +
+                 "   <script>" +
+                 "        $(document).ready(function () {" +
+                 "                $('#prefix" + Model.Name + "').change(function () {" +
+                 "                    var selectedDate = $(this).val();" +
+                 "                    $('#" + Model.Name + "').val(getGregorianDate(selectedDate));" +
+                 "                });" +
+                 "        });" +
+                 "   </script>" +
+                 "</div>"
+                );
+        }
+        public static string ToPersian(DateTime d,bool includeTime=false)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            return !includeTime? $"{pc.GetYear(d)}/{pc.GetMonth(d):00}/{pc.GetDayOfMonth(d):00}":
+                $"{pc.GetYear(d)}/{pc.GetMonth(d):00}/{pc.GetDayOfMonth(d):00} {d.Hour:00}:{d.Minute:00}";
+        }
+    }
+
+}
