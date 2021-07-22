03a549a0c977dee3e09c03573c78beb78383e442
Vahid Aria <Vahid@ARIA-LAPTOP>
2021-07-22 15:23:53 +0430

    a

--- Redmask.Taghelpers/ScriptHelper.cs
+++ Redmask.Taghelpers/ScriptHelper.cs
@@ -3,6 +3,7 @@
     public class ScriptHelper
     {
         public static string AddJquery() => "<script src='/lib/jquery/jquery.min.js'></script>";
+        public static string AddScript(string src) => $"<script src='{src}'></script>";
 
         public static string AddIfNotExsists(string src) =>
             $@" if(document.querySelector(""script[src='{src}']"")==undefined)
